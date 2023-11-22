7ef7a56b385a2667e9b8b95be60ab1b93b91a352
doogunwo <dgw0601@naver.com>
2023-11-22 12:02:37 +0000

    dir, file 정리

--- src/doogunwo/cd.c
+++
@@ -1,7 +0,0 @@
-int change_directory(const char *dirname) {
-    if (chdir(dirname) != 0) {
-        perror("Error changing directory");
-        return -1;
-    }
-    return 0;
-}
--- src/doogunwo/mkdir.c
+++
@@ -1,7 +0,0 @@
-int make_directory(const char *dirname){
-    if(mkdir(dirname, 0777)!=0){
-        perror("Eror create directory");
-        return -1;
-    }
-    return 0;
-}
